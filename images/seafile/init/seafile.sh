cd8f4a219583423ab9e5aef993c7898fb9e2a84b
Stancu Florin <niflostancu@gmail.com>
2016-12-15 22:45:51 +0200

    Added docker image for seafile.

---
+++ images/seafile/init/seafile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+log=/var/log/seafile.log
+
+function stop_server() {
+    kill $( ps ax | grep -E 'seafile-controller|ccnet-server|seaf-server' | grep -v grep | awk '{ print $1 }' | xargs )
+    exit 0
+}
+
+trap stop_server SIGINT SIGTERM
+
+# Fix for https://github.com/haiwen/seafile/issues/478, forward seafdav localhost-only port
+[[ "${WORKAROUND478}" =~ [Tt]rue ]] && socat TCP4-LISTEN:8080,fork TCP4:localhost:8081 &
+
+/opt/seafile/seafile-server-latest/seafile.sh start >> $log 2>&1
+
+# Script should not exit unless seafile died
+while pgrep -f "seafile-controller" 2>&1 >/dev/null; do
+    sleep 1;
+done
+
+exit 0
+
---
+++ images/seafile/init/seahub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+log=/var/log/seafile.log
+
+function stop_server() {
+  ps ax | grep run_gunicorn | awk '{ print $1 }' | xargs kill
+}
+
+trap stop_server SIGINT SIGTERM
+
+if [[ "${FASTCGI}" =~ [Tt]rue ]]
+then
+    SEAFILE_FASTCGI_HOST='0.0.0.0' /opt/seafile/seafile-server-latest/seahub.sh start-fastcgi >> $log
+else
+    /opt/seafile/seafile-server-latest/seahub.sh start >> $log 2>&1
+fi
+
+# Script should not exit unless seahub died
+while pgrep -f "manage.py run_gunicorn" 2>&1 >/dev/null; do
+    sleep 1;
+done
+
+exit 0
+
