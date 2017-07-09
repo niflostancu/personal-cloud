d7c2ae4be31b6023da475f1aff6202fe3ca01391
Stancu Florin <niflostancu@gmail.com>
2017-07-09 20:39:49 +0300

    frontend-nginx: fixed issues with alpine / docker.

--- images/frontend-nginx/init/nginx.sh
+++ images/frontend-nginx/init/nginx.sh
@@ -5,8 +5,6 @@ rm -f /var/log/nginx/error.log
 ln -s /dev/stderr /var/log/nginx/error.log
 ln -s /dev/stdout /var/log/nginx/access.log
 
-rm -f /etc/nginx/sites-enabled/* 2>/dev/null
-
 set -e
 
 OVERRIDES_DIR="/etc/nginx.overrides"
@@ -20,6 +18,7 @@ SECURE_SNIPPETS_ENDIR="/etc/nginx/snippets-enabled/default-ssl"
 INSECURE_SNIPPETS_ENDIR="/etc/nginx/snippets-enabled/default-ssl/insecure"
 
 # check if SSL is enabled
+rm -f /etc/nginx/sites-enabled/default-ssl /etc/nginx/sites-enabled/default
 if [[ "$SSL_ENABLED" =~ [Tt]rue ]]; then
     ln -s /etc/nginx/sites-available/default-ssl /etc/nginx/sites-enabled/default-ssl
     echo "Enabled default-ssl server (with SSL)"
@@ -30,10 +29,12 @@ fi
 
 # first, copy configuration overrides
 if [[ -d "$OVERRIDES_DIR" ]]; then
-    cp -r "$OVERRIDES_DIR/." "/etc/nginx/"
+    cp -rf "$OVERRIDES_DIR/." "/etc/nginx/"
 fi
 
 # activate default snippets
+rm -rf "$DEFAULT_SNIPPETS_ENDIR"
+rm -rf "$INSECURE_SNIPPETS_ENDIR"
 mkdir -p "$DEFAULT_SNIPPETS_ENDIR"
 mkdir -p "$INSECURE_SNIPPETS_ENDIR"
 
@@ -41,9 +42,9 @@ if [[ -n "$DEFAULT_SNIPPETS" ]]; then
     for snippet in $DEFAULT_SNIPPETS; do
         if [[ "$SSL_ENABLED" =~ [Tt]rue ]]; then
             if [[ -f "$SECURE_SNIPPETS_DIR/$snippet.conf" ]]; then
-                ln -s "$SECURE_SNIPPETS_DIR/$snippet.conf" "$SECURE_SNIPPETS_ENDIR/$snippet.conf"
+                ln -s "$SECURE_SNIPPETS_DIR/$snippet.conf" "$SECURE_SNIPPETS_ENDIR/$snippet.conf" || true
             elif [[ -f "$DEFAULT_SNIPPETS_DIR/$snippet.conf" ]]; then
-                ln -s "$DEFAULT_SNIPPETS_DIR/$snippet.conf" "$SECURE_SNIPPETS_ENDIR/$snippet.conf"
+                ln -s "$DEFAULT_SNIPPETS_DIR/$snippet.conf" "$SECURE_SNIPPETS_ENDIR/$snippet.conf" || true
             else
                 echo "WARNING: default-ssl snippet '$snippet' not found!"
             fi
