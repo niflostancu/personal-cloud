beaca48fc710da12922607069d7a487fe114429a
Stancu Florin <niflostancu@gmail.com>
2018-01-07 12:05:45 +0200

    mysql: modified backup cron hour + separate daily prefix

--- images/mysql/scripts/mysql-backup.sh
+++ images/mysql/scripts/mysql-backup.sh
@@ -5,6 +5,7 @@ if [[ -n "$MYSQL_BACKUP_USER" ]]; then
     mysqldump -u "$MYSQL_BACKUP_USER" -p"$MYSQL_BACKUP_PASSWORD" --all-databases \
         --single-transaction | gzip -9 > /var/backup/mysql.sql.gz
     chmod 600 /var/backup/mysql.sql.gz
+    cp -a /var/backup/mysql.sql.gz /var/backup/mysql.D.sql.gz
     cp -a /var/backup/mysql.sql.gz /var/backup/mysql.W.sql.gz
     cp -a /var/backup/mysql.sql.gz /var/backup/mysql.M.sql.gz
 
